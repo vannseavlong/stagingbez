70a6ad817a76b0919519ffb81257c2567e6510cd
Seiha <seiha.ly@suntel.io>
2025-11-14 12:02:42 +0700

    chore: remove unoptimized

--- next.config.ts
+++ next.config.ts
@@ -8,10 +8,10 @@ const nextConfig: NextConfig = {
         hostname: "**",
       },
     ],
-    unoptimized: false,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     formats: ["image/avif", "image/webp"],
+    minimumCacheTTL: 60 * 60 * 24, // 1 day caching for optimized images
   },
   reactStrictMode: true,
   poweredByHeader: false,
